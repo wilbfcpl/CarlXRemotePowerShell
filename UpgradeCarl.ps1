c013fe42f5176fc282382dd1441010b31505cab0
wilbfcpl <89823326+wilbfcpl@users.noreply.github.com>
2024-10-03 12:17:25 -0400

    Upgrade Script Added

---
+++ UpgradeCarl.ps1
@@ -0,0 +1,6 @@
+﻿# Close applications if they're running
+Get-Process | Where-Object { ($_.Name -Eq "Staff") -Or ($_.Name -Eq "ITS International") -Or ($_.Name -Eq "Admin") } | Stop-Process -Force
+
+
+# Start installer as a process and wait for it to finish
+Start-Process -FilePath "$PSScriptRoot\CarlX.exe" -ArgumentList "/VerySilent" -ErrorAction "SilentlyContinue" -Wait
